af1ce1783f0e9c05dd68d3dad851f0da2d607353
Frank Artur <frank_artur_25@hotmail.com>
2020-08-15 16:59:56 -0500

    subiendo los primeros archivos

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { Link } from './link/link.model'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  links: Link[];
+
+  constructor(){
+    this.links=[
+      new Link('Angular', 'http://angular.io', 100),
+      new Link('Google', 'http://angular.io', 100),
+    ];
+    console.log(this.links)
+  }
+  
+  agregarLink(title: HTMLInputElement, link: HTMLInputElement){
+    this.links.push(new Link(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+}
